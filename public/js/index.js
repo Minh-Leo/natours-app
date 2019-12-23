cac8f3b6d08f56ac5fadc2bf4cdc24c630f860a3
Minh Bui <tranduyminh.bui@gmail.com>
2019-12-23 21:58:19 +1030

    Implemented stripe message

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSetting';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM element
 const mapBox = document.getElementById('map');
@@ -65,3 +66,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 15);
