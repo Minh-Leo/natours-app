b443cc3f5582da3d0538d40b451cdc50b2483c57
Minh Bui <tranduyminh.bui@gmail.com>
2019-12-23 21:04:33 +1030

    implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARE
+// Implement CORS
+// allowed cors on simple req (get,post)
+app.use(cors());
+// allowed cors on complex request (put,patch,delete)
+app.options('*', cors());
+
 // Set security HTTP headers
 app.use(helmet());
 // Dev Logging
